170ea7cf8cf3c40e7968ad2d5917fb59eef48063
martin cailleau <martin.cailleau@gmail.com>
2020-10-18 16:04:35 +0200

    integration anim and timeline

---
+++ Assets/PositionXController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionXController : MonoBehaviour
+{
+    public bool Activate_OSC = true;
+    public string address;
+    public float rotX = 0;
+    public bool reverse = true;
+    public float rotOffset = 0;
+    public float dampSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OSC.instance.SetAddressHandler(address, OnReceiveRotX);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Slerp(transform.localRotation,Quaternion.Euler(new Vector3(-90, rotX, 0)),Time.deltaTime * dampSpeed);
+        
+    }
+
+    void OnReceiveRotX(OscMessage message)
+    {
+        Debug.Log(message);
+        if (reverse)
+        {
+            rotX = ((1-message.GetFloat(0)) * 360)+rotOffset;
+        }
+        else
+        {
+            rotX = (message.GetFloat(0) * 360) + rotOffset;
+        }
+
+
+    }
+}
